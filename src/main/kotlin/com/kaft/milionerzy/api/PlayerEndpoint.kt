065ad9d47d3240612fc96979336b96fe245ae1e1
l.smigielski <lukasz.smigielski@sciamus.com>
2020-07-10 18:03:20 +0200

    full post /players endpoint

--- src/main/kotlin/com/kaft/milionerzy/api/PlayerEndpoint.kt
+++ src/main/kotlin/com/kaft/milionerzy/api/PlayerEndpoint.kt
@@ -1,15 +1,36 @@
 package com.kaft.milionerzy.api
 
+import com.kaft.milionerzy.api.dto.PlayerCreatedResponse
 import com.kaft.milionerzy.api.dto.PlayerDto
+import com.kaft.milionerzy.domain.games.Game
+import com.kaft.milionerzy.domain.games.GameRepository
+import com.kaft.milionerzy.domain.games.GameStatus
 import com.kaft.milionerzy.domain.players.PlayerRepository
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import java.lang.RuntimeException
+import java.util.*
 
 @RestController
 @RequestMapping("/players")
-class PlayerEndpoint(val playerRepository: PlayerRepository) {
+class PlayerEndpoint(val playerRepository: PlayerRepository, val gameRepository: GameRepository) {
 
     @PostMapping
-    fun createPlayer(playerDto: PlayerDto) = playerRepository.insert(playerDto.createEntity())
+    fun createPlayer(@RequestBody playerDto: PlayerDto): PlayerCreatedResponse {
+        playerRepository.findById(playerDto.name).ifPresent {
+            throw PlayerExistsException("Name is already taken!")
+        }
+        gameRepository.findByGameStatus(GameStatus.ACTIVE).firstOrNull()?.let {
+            throw GameIsActiveException("Game is already in ACTIVE state!")
+        }
+        playerRepository.insert(playerDto.createEntity())
+        return gameRepository.findByGameStatus(GameStatus.CREATED).firstOrNull()?.let { PlayerCreatedResponse(it.id, GameStatus.CREATED, false) }
+                ?: gameRepository.insert(Game(UUID.randomUUID(), GameStatus.CREATED)).let { PlayerCreatedResponse(it.id, GameStatus.CREATED, true) }
+    }
 }
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+data class PlayerExistsException(val msg: String): RuntimeException(msg)
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+data class GameIsActiveException(val msg: String): RuntimeException(msg)
